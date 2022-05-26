2f76972ad071fd45d8968982a6772212de289bec
AbdullahJawaid <abdullahjawaid30@gmail.com>
2022-05-26 23:03:07 +0500

    adding some animation

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ const navbar = document.querySelector('.nav-bar');
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle('change');
 })
+
+const swiper = new Swiper('.swiper', {
+
+
+    autoplay: {
+
+        delay: 3000,
+        disableOnInteraction:false,
+    },
+
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
